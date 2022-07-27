dfe4d628f928c56542cbb2ac298b564e57fc0ccb
Lucas <lucas@mail.com>
2022-07-26 21:41:18 -0300

    ESLint configuration and lint fix.

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
 import express from 'express';
-import routes from './routes/url.js'
-import db from './db.js';
+import routes from './routes/url';
+import db from './db';
 
 (async () => {})();
-    db.sync();
+db.sync();
 
-    const app = express();
-    app.use(express.json());
-    app.use(routes);
+const app = express();
+app.use(express.json());
+app.use(routes);
 
-    const PORT = 3000;
+const PORT = 3000;
 
-    app.listen(PORT, () => {
-        console.log(`=> The server is running on port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`=> The server is running on port http://localhost:${PORT}`);
 });
-
-
-
