96b6a3acb797636cff046f4de932a33af0f04099
Lucas Quintanilha <l.quitan@gmail.com>
2022-07-27 20:40:55 -0300

    Revert "ESLint configuration and lint fix."
    This reverts commit dfe4d628f928c56542cbb2ac298b564e57fc0ccb.

--- src/models/Url.js
+++ src/models/Url.js
@@ -1,16 +1,16 @@
 import { Sequelize } from 'sequelize';
-import db from '../db';
+import db from '../db.js';
 
 const sequelize = db.define('url', {
-  alias: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    primaryKey: true,
-  },
-  url: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
+    alias: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
 });
 
 export default sequelize;
